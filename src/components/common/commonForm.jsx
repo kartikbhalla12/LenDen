4a8db2d872ff6a748d9b4e470d0286f7998397c1
Kartik <kartikbhalla12@gmail.com>
2020-06-07 00:49:08 +0530

    login: refractor code
    Signed-off-by: Kartik <kartikbhalla12@gmail.com>

---
+++ src/components/common/commonForm.jsx
@@ -0,0 +1,75 @@
+import React, { Component } from 'react';
+import Joi from 'joi-browser';
+import { Form, Alert } from 'react-bootstrap';
+import BarLoader from 'react-spinners/BarLoader';
+
+class CommonForm extends Component {
+	state = {
+		data: {},
+		errors: '',
+	};
+	handleChange = ({ currentTarget: input }) => {
+		const data = { ...this.state.data };
+		data[input.name] = input.value;
+
+		this.setState({ data });
+	};
+
+	validate = () => {
+		const { error } = Joi.validate(this.state.data, this.schema);
+		if (error) return error.details[0].message;
+		return '';
+	};
+
+	handleSubmit = (e) => {
+		e.preventDefault();
+
+		const error = this.validate();
+		this.setState({ error });
+		if (error) return;
+
+		this.doSubmit();
+	};
+
+	renderInput(name, placeholder) {
+		const { data } = this.state;
+		return (
+			<Form.Group controlId={name}>
+				<Form.Control
+					className='input'
+					name={name}
+					type={name}
+					placeholder={placeholder}
+					value={data[name]}
+					onChange={this.handleChange}
+				/>
+			</Form.Group>
+		);
+	}
+	renderAlert = () => {
+		const { error } = this.state;
+		return (
+			error && (
+				<Alert id='error-alert' variant='danger'>
+					{error}
+				</Alert>
+			)
+		);
+	};
+
+	renderLoader = () => {
+		return (
+			<BarLoader
+				css={{
+					display: 'block',
+					margin: '3vh auto 0 auto',
+				}}
+				size={50}
+				color={'#ef5350'}
+				loading={this.state.loading}
+			/>
+		);
+	};
+}
+
+export default CommonForm;
--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,10 +1,11 @@
-import React, { Component } from 'react';
+import React from 'react';
 import '../css/components/sign.css';
-import { Image, Form, Button, Alert } from 'react-bootstrap';
+import { Image, Form, Button } from 'react-bootstrap';
 import Joi from 'joi-browser';
-import BarLoader from 'react-spinners/BarLoader';
 
-class Login extends Component {
+import CommonForm from './common/commonForm';
+
+class Login extends CommonForm {
 	state = {
 		data: {
 			email: '',
@@ -19,77 +20,26 @@ class Login extends Component {
 		password: Joi.string().required().label('Password'),
 	};
 
-	handleSubmit = (e) => {
-		e.preventDefault();
-
-		const error = this.validate();
-		this.setState({ error });
-		if (error) return;
-
+	doSubmit = () => {
 		this.setState({ loading: true });
 		console.log('submitted');
 		this.props.history.push('/');
 	};
 
-	handleChange = ({ currentTarget: input }) => {
-		const data = { ...this.state.data };
-		data[input.name] = input.value;
-
-		this.setState({ data });
-	};
-
-	validate = () => {
-		const { error } = Joi.validate(this.state.data, this.schema);
-		if (error) return error.details[0].message;
-		return '';
-	};
-
 	render() {
-		const { data, error } = this.state;
-
 		return (
 			<div className='mainContainer'>
 				<div className='formBox'>
 					<Image src='/images/demoLogo.png' style={{ maxWidth: '150px' }} />
 					<h2>Please Sign In</h2>
 					<Form noValidate className='form' onSubmit={this.handleSubmit}>
-						<Form.Group controlId='formBasicEmail'>
-							<Form.Control
-								className='input'
-								name='email'
-								value={data.email}
-								onChange={this.handleChange}
-								type='email'
-								placeholder='Email Address'
-							/>
-						</Form.Group>
-						<Form.Group controlId='formBasicPassword'>
-							<Form.Control
-								className='input'
-								name='password'
-								value={data.password}
-								type='password'
-								placeholder='Password'
-								onChange={this.handleChange}
-							/>
-						</Form.Group>
-						{error && (
-							<Alert id='error-alert' variant='danger'>
-								{error}
-							</Alert>
-						)}
+						{this.renderInput('email', 'Email Address')}
+						{this.renderInput('password', 'Password')}
+						{this.renderAlert()}
 						<Button className='btn-log' variant='primary' type='submit'>
 							Sign In
 						</Button>
-						<BarLoader
-							css={{
-								display: 'block',
-								margin: '3vh auto 0 auto',
-							}}
-							size={50}
-							color={'#ef5350'}
-							loading={this.state.loading}
-						/>
+						{this.renderLoader()}
 					</Form>
 					<div className='separator'>New to LenDen ?</div>
 					<Button
