1e2a47d7ab8a54d9ee1b17b6166493064ba1a55f
Kartik <kartikbhalla12@gmail.com>
2020-06-10 13:21:28 +0530

    categories: add top heading
    Signed-off-by: Kartik <kartikbhalla12@gmail.com>

--- src/components/home/categories.jsx
+++ src/components/home/categories.jsx
@@ -7,27 +7,31 @@ class Categories extends Component {
 		return (
 			<div
 				className='container'
-				style={{
-					position: 'relative',
-					display: 'flex',
-					flexWrap: 'wrap',
-					justifyContent: 'center',
-				}}>
-				<CategoryElement
-					categoryTitle='GAMING'
-					categoryMessage='kirensd dsug hd uidshg aiud saui dusag dauisd uigsa d dsadasd dsa d'
-					src='/images/categories/gaming.png'
-				/>
-				<CategoryElement
-					categoryTitle='BOOKS'
-					categoryMessage='kirensd dsug hd uidshg aiud saui dusag dauisd uigsa d dsadasd dsa d'
-					src='/images/categories/book.png'
-				/>
-				<CategoryElement
-					categoryTitle='MOBILES'
-					categoryMessage='kirensd dsug hd uidshg aiud saui dusag dauisd uigsa d dsadasd dsa d'
-					src='/images/categories/mobile.png'
-				/>
+				style={{ textAlign: 'center', marginTop: '2vh' }}>
+				<h4 style={{ fontFamily: 'Balsamiq Sans' }}>Our various categories!</h4>
+				<div
+					style={{
+						position: 'relative',
+						display: 'flex',
+						flexWrap: 'wrap',
+						justifyContent: 'center',
+					}}>
+					<CategoryElement
+						categoryTitle='GAMING'
+						categoryMessage='kirensd dsug hd uidshg aiud saui dusag dauisd uigsa d dsadasd dsa d'
+						src='/images/categories/gaming.png'
+					/>
+					<CategoryElement
+						categoryTitle='BOOKS'
+						categoryMessage='kirensd dsug hd uidshg aiud saui dusag dauisd uigsa d dsadasd dsa d'
+						src='/images/categories/book.png'
+					/>
+					<CategoryElement
+						categoryTitle='MOBILES'
+						categoryMessage='kirensd dsug hd uidshg aiud saui dusag dauisd uigsa d dsadasd dsa d'
+						src='/images/categories/mobile.png'
+					/>
+				</div>
 			</div>
 		);
 	}
