5f9a2d1bd634ce70023c6369c49f798c70f15ef7
Kartik <kartikbhalla12@gmail.com>
2020-06-21 00:46:23 +0530

    index: disable strict mode to avoid issue with carousel
    Signed-off-by: Kartik <kartikbhalla12@gmail.com>

--- src/index.js
+++ src/index.js
@@ -8,9 +8,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
 	<BrowserRouter>
-		<React.StrictMode>
-			<App />
-		</React.StrictMode>
+		<App />
 	</BrowserRouter>,
 	document.getElementById('root')
 );
