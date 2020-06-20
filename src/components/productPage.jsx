d9f99d86058adc2339f28a4cc87974df5256bd57
Kartik <kartikbhalla12@gmail.com>
2020-06-21 00:24:18 +0530

    productPage: add carousel
    Signed-off-by: Kartik <kartikbhalla12@gmail.com>

--- src/components/productPage.jsx
+++ src/components/productPage.jsx
@@ -5,14 +5,26 @@ import _ from 'lodash';
 import '../css/components/common/productPage.css';
 import StarRating from 'react-star-rating-component';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faStar, faWallet } from '@fortawesome/free-solid-svg-icons';
+import {
+	faStar,
+	faWallet,
+	faHeart,
+	faExchangeAlt,
+	faAngleDoubleRight,
+	faAngleDoubleLeft,
+} from '@fortawesome/free-solid-svg-icons';
+import { Carousel } from 'react-bootstrap';
 
 class ProductPage extends Component {
 	state = {
 		product: {
 			name: 'Kitten hgasf  asfghd a sdhf hf dahs  dfsah dsa',
 			category: 'books',
-			src: 'http://placekitten.com/350/450',
+			src: [
+				'http://placekitten.com/350/450',
+				'http://placekitten.com/350/350',
+				'http://placekitten.com/350/850',
+			],
 			rating: 4,
 			ldc: 240,
 			wishlist: true,
@@ -26,6 +38,8 @@ class ProductPage extends Component {
 	};
 	componentDidMount = () => {
 		const { id } = this.props.match.params;
+		//api call using id
+		//populate state
 	};
 	render() {
 		const { src, category, name, rating, ldc, desc } = this.state.product;
@@ -45,18 +59,48 @@ class ProductPage extends Component {
 							width: '60%',
 							minWidth: '350px',
 						}}>
-						<Image
-							fluid
-							src={src}
-							style={{
-								display: 'block',
-								width: '100%',
-								height: '500px',
-								objectFit: 'cover',
-								maxWidth: 'inherit',
-								borderRadius: '5px',
-							}}
-						/>
+						<Carousel
+							interval={null}
+							nextIcon={
+								<FontAwesomeIcon
+									className='navIcon'
+									icon={faAngleDoubleRight}
+									style={{
+										color: '#ef5350',
+										fontSize: '30px',
+									}}
+								/>
+							}
+							prevIcon={
+								<FontAwesomeIcon
+									className='navIcon'
+									icon={faAngleDoubleLeft}
+									style={{
+										color: '#ef5350',
+										fontSize: '30px',
+									}}
+								/>
+							}>
+							{src.map((item) => {
+								return (
+									<Carousel.Item>
+										<Image
+											fluid
+											src={item}
+											style={{
+												display: 'block',
+												width: '100%',
+												height: '500px',
+												objectFit: 'cover',
+												maxWidth: 'inherit',
+												borderRadius: '5px',
+											}}
+										/>
+									</Carousel.Item>
+								);
+							})}
+						</Carousel>
+
 						<div style={{ display: 'flex', marginTop: '10px' }}>
 							<div
 								style={{
@@ -68,9 +112,18 @@ class ProductPage extends Component {
 									marginRight: '5px',
 									borderRadius: '5px',
 									fontFamily: 'Balsamiq Sans',
-									fontSize: '18px',
+									fontSize: '20px',
 									color: '#424242',
 								}}>
+								<FontAwesomeIcon
+									className='navIcon'
+									icon={faExchangeAlt}
+									style={{
+										color: '#ef5350',
+										fontSize: '20px',
+										marginRight: '5px',
+									}}
+								/>
 								BARTER
 							</div>
 							<div
@@ -86,78 +139,87 @@ class ProductPage extends Component {
 									fontSize: '18px',
 									color: '#424242',
 								}}>
-								WISHLIST
+								<FontAwesomeIcon
+									className='navIcon'
+									icon={faHeart}
+									style={{
+										color: '#fff',
+										fontSize: '25px',
+									}}
+								/>
+								{/* WISHLIST */}
 							</div>
 						</div>
 					</div>
 				</div>
 				<div style={{ flexBasis: '60%', marginLeft: '20px' }}>
-					<Breadcrumb
-						listProps={{
-							style: {
-								backgroundColor: 'transparent',
-								fontSize: '15px',
-								fontWeight: '500',
-								marginBottom: '1vh',
-								padding: 0,
-							},
-						}}>
-						<LinkContainer to='/'>
-							<Breadcrumb.Item>Home</Breadcrumb.Item>
-						</LinkContainer>
+					<div style={{ maxWidth: 750 }}>
+						<Breadcrumb
+							listProps={{
+								style: {
+									backgroundColor: 'transparent',
+									fontSize: '15px',
+									fontWeight: '500',
+									marginBottom: '1vh',
+									padding: 0,
+								},
+							}}>
+							<LinkContainer to='/'>
+								<Breadcrumb.Item>Home</Breadcrumb.Item>
+							</LinkContainer>
 
-						<Breadcrumb.Item active>{_.upperFirst(category)}</Breadcrumb.Item>
-					</Breadcrumb>
-					<div
-						style={{
-							fontFamily: 'Balsamiq Sans',
-							color: '#424242',
-							fontWeight: 400,
-							fontSize: '25px',
-						}}>
-						{name}
-					</div>
-					<StarRating
-						name='rate2'
-						editing={false}
-						renderStarIcon={() => (
+							<Breadcrumb.Item active>{_.upperFirst(category)}</Breadcrumb.Item>
+						</Breadcrumb>
+						<div
+							style={{
+								fontFamily: 'Balsamiq Sans',
+								color: '#424242',
+								fontWeight: 400,
+								fontSize: '25px',
+							}}>
+							{name}
+						</div>
+						<StarRating
+							name='rate2'
+							editing={false}
+							renderStarIcon={() => (
+								<FontAwesomeIcon
+									icon={faStar}
+									style={{
+										fontSize: '20px',
+										marginRight: '5px',
+										marginTop: '10px',
+									}}
+								/>
+							)}
+							starCount={5}
+							value={rating}
+							starColor='#ef5350'
+							emptyStarColor='#424242'
+						/>
+						<div
+							style={{
+								fontFamily: 'Balsamiq Sans',
+								fontSize: '30px',
+							}}>
 							<FontAwesomeIcon
-								icon={faStar}
+								className='navIcon'
+								icon={faWallet}
 								style={{
-									fontSize: '20px',
-									marginRight: '5px',
-									marginTop: '10px',
+									marginRight: '10px',
+									// fontSize: '16px',
 								}}
 							/>
-						)}
-						starCount={5}
-						value={rating}
-						starColor='#ef5350'
-						emptyStarColor='#424242'
-					/>
-					<div
-						style={{
-							fontFamily: 'Balsamiq Sans',
-							fontSize: '30px',
-						}}>
-						<FontAwesomeIcon
-							className='navIcon'
-							icon={faWallet}
+							{ldc}
+						</div>
+						<div
 							style={{
-								marginRight: '10px',
-								// fontSize: '16px',
-							}}
-						/>
-						{ldc}
-					</div>
-					<div
-						style={{
-							maxWidth: 750,
-							color: '#616161',
-							fontStyle: 'italic',
-							marginTop: '10px',
-						}}>
-						{desc}
+								color: '#616161',
+								fontStyle: 'italic',
+								marginTop: '10px',
+							}}>
+							{desc}
+						</div>
 					</div>
 				</div>
 			</div>
