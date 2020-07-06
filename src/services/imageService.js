aeb7a3adcdec3d26a0364cdadf0fa032ba531ba3
Kartik B <kartikbhalla12@gmail.com>
2020-07-06 16:51:50 +0530

    imageService: remove Content-Type
    Signed-off-by: Kartik B <kartikbhalla12@gmail.com>

--- src/services/imageService.js
+++ src/services/imageService.js
@@ -1,16 +1,12 @@
 import http from './httpService';
 import { api } from '../config.json';
-
-const apiEndPoint = `${api}/users/upload/image`;
+const apiEndPoint = `${api}/products/upload/image`;
 
 export default async function uploadImages(pictures) {
 	const fd = new FormData();
-	pictures.forEach(pic => fd.append('image', pic));
+	pictures.forEach(pic => fd.append('image', pic, pic.name));
 
 	return http.post(apiEndPoint, fd, {
-		headers: {
-			'Content-Type': 'multipart/form-data',
-			'Access-Control-Allow-Origin': '*',
-		},
+		headers: {},
 	});
 }
