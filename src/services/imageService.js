924a16a5da2192397fdfeadc06ed0f6795ec35f5
Kartik B <kartikbhalla12@gmail.com>
2020-07-07 13:37:52 +0530

    api: update api url
    Signed-off-by: Kartik B <kartikbhalla12@gmail.com>

--- src/services/imageService.js
+++ src/services/imageService.js
@@ -1,6 +1,6 @@
 import http from './httpService';
 import { api } from '../config.json';
-const apiEndPoint = `${api}/products/upload/image/`;
+const apiEndPoint = `${api}/products/upload/image`;
 
 export default async function uploadImages(pictures) {
 	const fd = new FormData();
