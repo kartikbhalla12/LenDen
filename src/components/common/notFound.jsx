7cce6ff65c8cd8d24b7f78984b5eb92e98fa5bee
Kartik B <kartikbhalla12@gmail.com>
2020-07-14 15:11:34 +0530

    not-found: design 404 page
    Signed-off-by: Kartik B <kartikbhalla12@gmail.com>

--- src/components/common/notFound.jsx
+++ src/components/common/notFound.jsx
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
+import { Image } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 class NotFound extends Component {
 	state = {};
 	render() {
-		return <h1>404</h1>;
+		return (
+			<div className='notFoundContainer'>
+				<div className='notFoundInnerContainer'>
+					<Image src='images/logo.png' />
+					<p>
+						There's nothing here, Go back to <Link to='/'>Home</Link> ?
+					</p>
+				</div>
+			</div>
+		);
 	}
 }
 
